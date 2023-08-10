b55a396574a85cffc20b46c9c1b5c6104e006b37
cagomni <cagomni@oocl.com>
2023-08-10 16:46:15 +0800

    my first commit in java exam

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -2,6 +2,26 @@ package com.tw;
 
 public class StringCalculator {
     public int add(String string) {
-        return 0;
-    }
-}
+
+        if (string.isEmpty()){
+            return 0;
+        }
+            String delimiter = ","; // my initial value for delimiter
+
+            if (string.startsWith("//")){
+                int indexNewLine = string.indexOf("\n"); // index is 4
+                delimiter = string.substring(2,indexNewLine); // new delimiter val ";" : index is 3
+                string = string.substring(indexNewLine + 1); // index is (4+1)
+            }
+
+            String[] numbers = string.split ( "[\n" +delimiter+ "]");
+            int sum =0;
+
+            for ( String num : numbers) {
+                sum += Integer.parseInt(num);
+            }
+            return sum;
+            }
+        }
+
+
